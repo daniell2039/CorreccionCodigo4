7da809470f15d5a1d1ee3ba19457166c9d1e7ee2
Daniel Leon <danirl.99.117@gmail.com>
2023-09-08 10:58:29 -0600

    first commit

---
+++ src/com/generation/Codigo4.java
@@ -0,0 +1,44 @@
+package com.generation;
+//Importar Scanner
+import java.util.Scanner;
+
+public class Codigo4 {
+
+    public static void main(String[] args) {
+
+
+    Scanner s = new Scanner(System.in); // Se necesita el source
+
+    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
+    String j1 = s.nextLine();
+
+    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
+    Scanner s2 = new Scanner(System.in); // Se necesita el source
+    String j2 = s.nextLine();
+
+    // Parentesis mal colocado
+    if (j1.equals(j2)) {  // se tiene que agregar equals
+        System.out.println("Empate");
+    } else {
+        int g = 2;
+        switch(j1) {
+            case "piedra":
+                if (j2.equals("tijeras")) {
+                    g = 1;
+                }
+
+            case "papel":
+                if (j2.equals("piedra")) {
+                    g = 1;
+                }
+            case "tijera":
+                if (j2.equals("papel")) {
+                    g = 1;
+                }
+                break;
+            default:
+        }System.out.println("Gana el jugador " + g); // se corrigio la posicion de este print
+
+    }
+    }
+}
